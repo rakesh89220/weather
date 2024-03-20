e22884534bafa32f5993203af33e5dc16bcc9759
rakesh89220 <rakeshyadav7852@gmail.com>
2024-03-20 22:20:54 +0530

    my app

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from "react";
+import Weathercard from "./Weathercard";
+import "./App.css";
+const Temp = () => {
+   const [searchValue, setSearchValue] = useState("Lucknow");
+   const[tempInfo, setTempInfo] = useState({});
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=e64e3b1f68d3ec7be6fdce54155a6fb3`;
+      const res = await fetch(url);
+       const data = await res.json();
+
+       const { temp,humidity,pressure } = data.main;
+       const { main: weathermood } = data.weather[0];
+       const { name } = data;
+       const {  speed } = data.wind;
+       const { country, sunset } = data.sys;
+
+       const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+      setTempInfo(myNewWeatherInfo );
+     }
+    catch (error) {
+      console.log(error);
+    }
+   };
+   useEffect(() => {
+    getWeatherInfo();
+   },[]);
+   return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="search..."
+            autoFocus
+            id="search" 
+            className="searchTerm"
+             value={searchValue }
+             onChange={(e) => setSearchValue(e.target.value) }
+          />
+          <button className="searchButton" type="button" onClick={getWeatherInfo}>
+            Search
+          </button>
+        </div>
+      </div>
+      <Weathercard tempInfo={tempInfo}/>
+    </>
+  );
+};
+
+export default Temp;
+
